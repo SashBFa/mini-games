fcbee38973ae701b6e7c7489d499dbff8effe3a8
SashBFa <bastienfajs@gmail.com>
2023-03-16 15:50:51 +0100

    folder init

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export const App = () => {
+  return (
+    <div className="w-screen min-h-screen flex items-center justify-center bg-gradient-to-br from-slate-800 to-slate-900">
+      <p className="text-white font-black text-8xl">Hello there</p>
+    </div>
+  );
+};
